6bcfa42aa5db5ab51ee21d5b9c70670233ff0145
wuyuzaddd <1195150139@qq.com>
2025-11-26 15:45:11 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.wyzaddd.win"];
+pub const RS_PUB_KEY: &str = "dFkP1VQiYtrc6e+bljmFYtub+jWzoXc6uZ7Le4iBML8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
